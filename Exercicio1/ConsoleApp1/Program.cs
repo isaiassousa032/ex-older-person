fc9ba44199ce63342338c4652479668a5c0093ba
Isaias Santos <isaiassousa032@gmail.com>
2023-04-21 17:00:42 -0300

    Add files via upload

---
+++ Exercicio1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            Pessoa Pessoa1 = new Pessoa();
+            Pessoa Pessoa2 = new Pessoa();
+
+            Console.WriteLine("Dados da Primeira Pessoa:");
+            Console.Write("Nome: ");
+            Pessoa1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            Pessoa1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            Pessoa2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            Pessoa2.Idade = int.Parse(Console.ReadLine());
+
+            if(Pessoa1.Idade > Pessoa2.Idade)
+            {
+                Console.WriteLine($"Pessoa mais velha: {Pessoa1.Nome}");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa mais velha: {Pessoa2.Nome}");
+            }
+        }
+    }
+}
